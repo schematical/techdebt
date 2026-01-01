8ae91899cdc5d7e44563abd2b699bcb1fc2383c5
Matt Lea <matt@schematical.com>
2026-01-01 15:24:38 -0600

    Adding NetworkPacket Cost

--- TechDebt/Assets/Scripts/NetworkConnection.cs
+++ TechDebt/Assets/Scripts/NetworkConnection.cs
@@ -9,6 +9,8 @@ public class NetworkConnection
     public string TargetID;
     public int Priority = 5;
     public NetworkPacketData.PType networkPacketType;
+    public int Possiblity = 1;
+    public int Cost = 0;
     public List<NetworkConnectionBonus> NetworkConnectionBonus = new List<NetworkConnectionBonus>();
   
     public NetworkConnection(string targetID, NetworkPacketData.PType _networkPacketType)
