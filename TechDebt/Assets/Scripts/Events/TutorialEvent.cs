61c91815da63f506b8efe5cb20a287bd71a34b82
Matt Lea <matt@schematical.com>
2026-01-07 15:21:38 -0600

    Starting on TutorialEvent.cs

---
+++ TechDebt/Assets/Scripts/Events/TutorialEvent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Events
+{
+    public class TutorialEvent
+    {
+
+        public TutorialEvent()
+        {
+            
+        }
+        /*public virtual void Apply()
+        {
+            if (!string.IsNullOrEmpty(EventStartText))
+            {
+                GameManager.Instance.UIManager.ShowAlert(EventStartText);
+            }
+        }
+        
+        public virtual void End()
+        {
+            if (!string.IsNullOrEmpty(EventEndText))
+            {
+                GameManager.Instance.UIManager.ShowAlert(EventEndText);
+            }
+            GameManager.Instance.EndEvent(this);
+        }
+
+        public virtual bool IsPossible()
+        {
+            return true;
+        }
+        public virtual bool IsOver()
+        {
+            return true;
+        }*/
+    }
+
+  
+}
