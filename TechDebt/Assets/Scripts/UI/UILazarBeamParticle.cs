fc807dc48e4a2e608b3fff488ef2e72fca710531
Matt Lea <matt@schematical.com>
2026-01-24 12:40:27 -0600

    UIRewardPanel is looking pretty good.

--- TechDebt/Assets/Scripts/UI/UILazarBeamParticle.cs
+++ TechDebt/Assets/Scripts/UI/UILazarBeamParticle.cs
@@ -8,6 +8,17 @@ namespace UI
         void Update()
         {
             rectTransform.position = new Vector2(rectTransform.position.x, rectTransform.position.y + 1f);
+            
+            Vector3[] corners = new Vector3[4];
+            rectTransform.GetWorldCorners(corners);
+            
+            // Bottom-right corner is corners[2]
+            Vector3 bottomRight = corners[2];
+
+            if (bottomRight.x < 0 || bottomRight.x > Screen.width || bottomRight.y < 0 || bottomRight.y > Screen.height)
+            {
+                gameObject.SetActive(false);
+            }
         }
         public void Init(float rotationZ = 0)
         {
