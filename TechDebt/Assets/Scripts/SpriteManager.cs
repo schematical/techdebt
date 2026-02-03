800ad2b53b61a859408ae4323fc94a2fef50b7b0
Matt Lea <matt@schematical.com>
2026-02-03 09:49:03 -0600

    Fixing a build error.

--- TechDebt/Assets/Scripts/SpriteManager.cs
+++ TechDebt/Assets/Scripts/SpriteManager.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Unity.VisualScripting;
-using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.U2D.Animation;
 using Random = UnityEngine.Random;
