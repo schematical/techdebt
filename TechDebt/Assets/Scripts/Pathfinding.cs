2980a3346d53a33402a32ec25823db3aef6dbecb
Matt Lea <matt@schematical.com>
2026-02-03 08:43:52 -0600

    Minor tweaks.

--- TechDebt/Assets/Scripts/Pathfinding.cs
+++ TechDebt/Assets/Scripts/Pathfinding.cs
@@ -106,3 +106,23 @@ public static class Pathfinding
         return 14 * dstX + 10 * (dstY - dstX);
     }
 }
+public class Node
+{
+    public int gridX, gridY;
+    public bool isWalkable;
+    public Vector3 worldPosition;
+
+    public int gCost; // Cost from the starting node
+    public int hCost; // Heuristic cost to the end node
+    public Node parent; // The previous node in the path
+
+    public int fCost { get { return gCost + hCost; } }
+
+    public Node(bool _isWalkable, Vector3 _worldPos, int _gridX, int _gridY)
+    {
+        isWalkable = _isWalkable;
+        worldPosition = _worldPos;
+        gridX = _gridX;
+        gridY = _gridY;
+    }
+}
