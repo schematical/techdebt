3432bd6548631eeefdedc9ce908568e58955123d
Matt Lea <matt@schematical.com>
2026-02-23 13:02:25 -0600

    UIProductRoadMap.cs now renders but no connectors and no current state.

--- TechDebt/Assets/Scripts/UI/UIProductRoadMap.cs
+++ TechDebt/Assets/Scripts/UI/UIProductRoadMap.cs
@@ -1,19 +1,36 @@
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace UI
 {
     public class UIProductRoadMap: UIPanel
     {
+        public List<UIProductRoadMapLevelButton> Buttons = new List<UIProductRoadMapLevelButton>();
+        public RectTransform RectTransform;
+        public TextMeshProUGUI LevelDescriptionText;
+        protected override void Start()
+        {
+            base.Start();
+            
+        }
+
         public void Show()
         {
             base.Show();
             ProductRoadMap Map = GameManager.Instance.ProductRoadMap;
             int stageX = 0;
-            RectTransform rect = GetComponent<RectTransform>();
-            float width = rect.rect.width;
-            float height = rect.rect.height;
-
+            float width = RectTransform.rect.width;
+            float height = RectTransform.rect.height;
+            foreach (UIProductRoadMapLevelButton button in Buttons)
+            {
+                button.gameObject.SetActive(false);
+            }
+            Buttons.Clear();
+            
+            
             foreach (ProductRoadMapStage stage in Map.Stages)
             {
                 int levelY = 0;
@@ -28,14 +45,21 @@ namespace UI
                     float x = (width / Map.Stages.Count) * (stageX + 0.5f) - width / 2;
                     float y = (height / stage.Levels.Count) * (levelY + 0.5f) - height / 2;
                     button.GetComponent<RectTransform>().anchoredPosition = new Vector2(x, y);
-
+                    
+                    Buttons.Add(button);
                     levelY += 1;
                 }
 
                 stageX += 1;
             }
         }
+        public void OnHover(PointerEventData eventData)
+        {
+            Debug.Log("OnHover");
+        }
     }
+
+  
 
   
 }
