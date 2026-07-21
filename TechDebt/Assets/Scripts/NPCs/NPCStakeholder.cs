cf7d1909bfc5a9474a7461d7c83e1babf1e03863
Matt Lea <matt@schematical.com>
2026-07-21 14:57:34 -0500

    Added stake holder dialog.

--- TechDebt/Assets/Scripts/NPCs/NPCStakeholder.cs
+++ TechDebt/Assets/Scripts/NPCs/NPCStakeholder.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using System.Linq;
 using Infrastructure;
 using NPCs;
+using UI;
+using UnityEngine.EventSystems;
 
 public class NPCStakeholder : NPCAnimatedBiped
 {
@@ -16,7 +18,19 @@ public class NPCStakeholder : NPCAnimatedBiped
         this.home = home;
      
     }
-
+    public override void OnLeftClick(PointerEventData eventData)
+    {
+        // GameManager.Instance.UIManager.npcDetailPanel.Show(this);
+        RenderDetailBubble();
+    }
+    public virtual UIDialogBubble RenderDetailBubble()
+    {
+        UIDialogBubble dialogBubble = ShowDialogBubble();
+        dialogBubble.AddLine<UIPanelLine>().Add<UIPanelLineSectionText>().h1($"Hi! I am the {stakeholder.DisplayName}. \nHaving me on staff unlocks additional quests accessible via the product road map.");
+        dialogBubble.AddButton("Continue", () => dialogBubble.Close());
+        GameManager.Instance.UIManager.MarkDialogBubbleFocused(dialogBubble);
+        return dialogBubble;
+    }
     public override void TriggerDefaultBehavior()
     {
         
